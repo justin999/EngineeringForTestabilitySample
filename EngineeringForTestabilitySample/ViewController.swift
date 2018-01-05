ae23d8fc89f7edf5fea16164737c9d853d6c8fc3
justin999 <koichi.sato.aow@gmail.com>
2018-01-05 20:54:49 +0900

    Initial Commit

---
+++ EngineeringForTestabilitySample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EngineeringForTestabilitySample
+//
+//  Created by Koichi Sato on 2018/01/05.
+//  Copyright © 2018 justin999. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
